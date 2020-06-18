f8795772e448d26aca7b074f7fb813aa89bd8935
dvfleet413 <dvfleet413@gmail.com>
2020-06-18 11:16:32 -0400

    configured Startup class"

--- DavesPieShop/Startup.cs
+++ DavesPieShop/Startup.cs
@@ -16,24 +16,51 @@ namespace DavesPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // register services here through Dependency Injection
+            // through DI we can acheive a a loose coupling architecture for our project
+            // tight coupling requires hard interactions between classes, adding an interface helps a little
+            // DI container handles creating instances of classes, it is the place where we register all of our dependencies
+            // everyone knows about the DI container, and can ask it for what it needs
+            // This method is where we register framework servives and all of our own services
+
+            // framework services
+            services.AddControllersWithViews();
+
+            // our own services
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Add middleware here
+            // this is the reuest pipeline, a number of components chained to one another
+            // these components intercept and handle http requests, and create http response
+            // each component (middleware) can alter the request/response and pass it to the next component
+            
+            // the order in which middleware is listed is important!
+
             if (env.IsDevelopment())
             {
+                // this middleware enables use of exception pages in development, which provides us with useful information
                 app.UseDeveloperExceptionPage();
             }
 
+            // redirects HTTP to HTTPS
+            app.UseHttpsRedirection();
+            // this middleware enables ability for site to serve static files
+            app.UseStaticFiles();
+            // this middleware adds support for text only headers for status codes
+            app.UseStatusCodePages();
+
             app.UseRouting();
 
+            // this middleware enables routing the request to the correct endpoint
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // this has been modified to handle routing in a more MVC way, instead of routing everything to "Hello World"
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{actions=Index}/{id?}");
             });
         }
     }

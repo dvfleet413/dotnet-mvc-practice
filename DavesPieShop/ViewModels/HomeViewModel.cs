675e146183688a0f5735d08346e2b8115eea9a6e
dvfleet413 <dvfleet413@gmail.com>
2020-06-20 11:33:52 -0400

    added home cotroller with index view

---
+++ DavesPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using DavesPieShop.Models;
+
+namespace DavesPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
